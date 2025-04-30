6a12f204b09ad816d79b8fe0980d946672eb6cb2
microswap-lang <candlrinc@gmail.com>
2025-04-30 10:47:58 -0600

    Update IPancakeFactory.sol

--- contracts/interfaces/IPancakeFactory.sol
+++ contracts/interfaces/IPancakeFactory.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 interface IPancakeFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
@@ -7,7 +8,7 @@ interface IPancakeFactory {
     function feeToSetter() external view returns (address);
 
     function getPair(address tokenA, address tokenB) external view returns (address pair);
-    function allPairs(uint) external view returns (address pair);
+    function allPairs(uint index) external view returns (address pair);
     function allPairsLength() external view returns (uint);
 
     function createPair(address tokenA, address tokenB) external returns (address pair);
